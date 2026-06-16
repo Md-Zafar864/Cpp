7ad8dcf1b0a786cc5278e28fa96b4e261e02eabb
Md-Zafar864 <mdzafar313544@gmail.com>
2026-06-16 14:22:39 +0530

    updated solution

--- code/Search_in_Rotated_Sorted_array.cpp
+++ code/Search_in_Rotated_Sorted_array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
+        int n=nums.size();//size
         int s=0, e=n-1;
         int mid;
         while(s<=e){
